f223667621d076f1e1dbbc748653aae391343b96
Wahyunursahid <101644756+Wahyunursahid@users.noreply.github.com>
2024-05-20 13:24:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def set_bg_img(url):
     )
 
 # Use this function at the top of your main Streamlit script
-set_bg_img("https://th.bing.com/th?id=OIP.QXvuPVGx27AKlF7HAyUipAHaLH&w=204&h=306&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2")  # Replace with your actual image URL
+set_bg_img("https://th.bing.com/th/id/OIP.dxNchr1F7Gf3oOxx64ALvAHaDt?w=349&h=175&c=7&r=0&o=5&dpr=1.3&pid=1.7")  # Replace with your actual image URL
 
 # Define symptoms and their associated diseases with expert CF values
 symptoms = {
