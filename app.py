59de9e525a5fb7ea9fb868324d500f4a37b02621
Wahyunursahid <101644756+Wahyunursahid@users.noreply.github.com>
2024-05-20 13:04:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def add_bg_from_url():
         f"""
         <style>
         .stApp {{
-            background-image: url("/background.jpg");
+            background-image: url("https://github.com/Wahyunursahid/penyakit-kulit/blob/main/background.jpg");
             background-size: cover;
         }}
         </style>
